6ecece458d628154de69ceb322d80755ed8598d5
Dammar bhatt <dammarbhatt111@gmail.com>
2026-07-19 09:21:14 +0545

    update: path alies setup and theame toggle implemented

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,13 +1,17 @@
-import { Outlet, Link } from 'react-router-dom';
+import { ModeToggle } from "@/components/mode-toggle";
+import { Outlet, Link } from "react-router-dom";
 
 function MainLayout() {
   return (
     <div className="min-h-screen flex flex-col">
       <header className="border-b p-4 flex gap-4">
-        <Link to="/" className="font-bold text-primary">ShopWave</Link>
+        <Link to="/" className="font-bold text-primary">
+          ShopWave
+        </Link>
         <Link to="/products">Products</Link>
         <Link to="/cart">Cart</Link>
         <Link to="/login">Login</Link>
+        <ModeToggle />
       </header>
 
       <main className="flex-1">
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { store } from "./store/store.ts";
 import { Provider } from "react-redux";
+import { ThemeProvider } from "./components/theme-provider.tsx";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider defaultTheme="light" storageKey="shopwave-theme">
+        <App />
+      </ThemeProvider>
     </Provider>
   </StrictMode>,
 );
